4a33a29922aec9876c416ca43f01a5a09de0fbf2
Sufiyansiddiqui123 <89350881+Sufiyansiddiqui123@users.noreply.github.com>
2025-03-27 20:06:58 +0000

    Setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const emailRoutes = require("./routes/emails");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/emails", emailRoutes);
+
+module.exports = app;
